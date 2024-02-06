b8865edcd7864315efb7e2b05629d65a8aba3169
Dhrumil <dhrumilbhut@gmail.com>
2024-02-06 14:15:13 +0530

    fix

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ const bot = new TelegramBot(token, { polling: true });
 
 const app = express();
 
+app.use('/', (req, res) => {
+  res.send("Hello World!");
+});
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
